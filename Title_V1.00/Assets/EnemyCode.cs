191443f03de9ed95c5bd25a4c4c22045ecfdaba8
theodoreEL <34727776+theodoreEL@users.noreply.github.com>
2018-02-03 16:40:33 -0600

    Small Level
    Temp Player movement
    Collision with walls
    Enemy detection and some movement

---
+++ Title_V1.00/Assets/EnemyCode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCode : MonoBehaviour {
+
+    public float range;
+    public Transform Player;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+		if(Vector2.Distance(Player.position, transform.position) <= range)
+        {
+            //transform.Translate(Vector2.MoveTowards(transform.position, -Player.position, 0) * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, Player.position, step);
+        }
+	}
+}
